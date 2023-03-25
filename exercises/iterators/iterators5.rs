6f13c2eabad4bf740708a54f210e2516f0edd2ce
Aleksandar Stanisic <aleksandar@tuta.io>
2023-03-25 13:03:57 +0100

    At 76. I have to learn more to continue
    Mutex, Arc, smart pointers.
    After that are macros.

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -10,8 +10,6 @@
 //
 // Make the code compile and the tests pass.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -32,9 +30,10 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 }
 
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
+    map.iter().filter(|v| v.1 == &value).count()
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // todo!();
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -50,10 +49,13 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
 }
 
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
+    collection
+        .iter()
+        .map(|map| count_iterator(map, value))
+        .fold(0, |acc, x| acc + x)
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
 }
 
 #[cfg(test)]
